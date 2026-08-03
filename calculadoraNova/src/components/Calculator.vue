05170962bc5336a3550994c0f70df6c6c341559a
Gilberto Junior <gilberto-junior@outlook.com>
2026-08-03 19:55:37 -0300

    Alterando lógica de cálculos

--- calculadoraNova/src/components/Calculator.vue
+++ calculadoraNova/src/components/Calculator.vue
@@ -2,6 +2,7 @@
 import {ref} from 'vue';
 
 const current = ref("");
+const stringCalculo = ref("");
 const operator = ref(null);
 const previous = ref(null);
 const operatorClicked = ref(false);
@@ -17,48 +18,25 @@ const percent = () => {
   current.value = `${parseFloat(current.value) / 100}`
 }
 
-const append = (number) => {
+const append = (valor) => {
   if (operatorClicked.value) {
     current.value = ''
     operatorClicked.value = false
   }
-  current.value = `${current.value}${number}` //irá adicionando o n° no fim da string
+  current.value += valor; //irá adicionando o n° no fim da string
 }
 
-const dot = () => {
-  if (current.value.indexOf('.') === -1) {
-    append.value('.')
-  }
-}
-
-const setPrevious = () => {
-  previous.value = current.value;
+const setOperator = (operator) => {
+  stringCalculo.value += current.value;
+  current.value = operator;
   operatorClicked.value = true;
-}
-
-const divide = () => {
-  operator.value = (a, b) => a / b
-  setPrevious()
-}
-
-const times = () => {
-  operator.value = (a, b) => a * b
-  setPrevious()
-}
-
-const minus = () => {
-  operator.value = (a, b) => a - b
-  setPrevious()
-}
-
-const add = () => {
-  operator.value = (a, b) => a + b
-  setPrevious()
+  stringCalculo.value += operator;
 }
 
 const equal = () => {
-  current.value = `${operator.value(parseFloat(current.value), parseFloat(previous.value))}`
-  previous.value = null;
+  stringCalculo.value += current.value;
+  current.value = eval(stringCalculo.value);
+  stringCalculo.value = '';
 }
 </script>
 
@@ -68,21 +46,21 @@ const equal = () => {
     <div @click="clear" class="btn">C</div>
     <div @click="sign" class="btn">+/-</div>
     <div @click="percent" class="btn">%</div>
-    <div @click="divide" class="btn operator">/</div>
+    <div @click="setOperator('/')" class="btn operator">/</div>
     <div @click='append(7)' class="btn">7</div>
     <div @click='append(8)' class="btn">8</div>
     <div @click='append(9)' class="btn">9</div>
-    <div @click="times" class="btn operator">x</div>
+    <div @click="setOperator('*')" class="btn operator">x</div>
     <div @click='append(4)' class="btn">4</div>
     <div @click='append(5)' class="btn">5</div>
     <div @click='append(6)' class="btn">6</div>
-    <div @click="minus" class="btn operator">-</div>
+    <div @click="setOperator('-')" class="btn operator">-</div>
     <div @click='append(1)' class="btn">1</div>
     <div @click='append(2)' class="btn">2</div>
     <div @click='append(3)' class="btn">3</div>
-    <div @click="add" class="btn operator">+</div>
+    <div @click="setOperator('+')" class="btn operator">+</div>
     <div @click='append(0)' class="zero btn">0</div>
-    <div @click="dot()" class="btn">.</div>
+    <div @click="append(',')" class="btn">.</div>
     <div @click="equal" class="btn operator">=</div>
   </div>
 </template>
